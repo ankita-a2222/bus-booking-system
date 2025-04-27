9811051d145753b6b47eb13232bfd7f48dc5ef8b
ankita-a2222 <160044383+ankita-a2222@users.noreply.github.com>
2025-04-27 19:15:39 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,9 @@
+{  
+  "name": "bus-booking-system",  
+  "version": "1.0.0",  
+  "description": "Bus Booking System",  
+  "dependencies": {  
+    "express": "^4.17.1",  
+    "serverless-http": "^2.7.0"  
+  }  
+}  
